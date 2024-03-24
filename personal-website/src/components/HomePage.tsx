00d47071f67d4e730f2074ea3ebe861d1df318bf
Hannah Hsiao <hhsiao24@gmail.com>
2024-03-24 04:08:42 -0400

    home animations

--- personal-website/src/components/HomePage.tsx
+++ personal-website/src/components/HomePage.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   Image,
   Container,
@@ -14,16 +15,28 @@ import image from "./snoopyWelcome.png";
 import classes from "./HomePage.module.css";
 
 export function HomePage() {
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAnimate(true);
+      console.log("Animation started");
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <Container size="md">
       <div className={classes.inner}>
-        <div className={classes.content}>
-          <Title className={classes.title}>
-            {/* A <span className={classes.highlight}>modern</span> React <br />{" "}
-            components library */}
+        <div className={`${classes.content} ${animate && classes.animate}`}>
+          <Title className={`${classes.title} ${animate && classes.animate}`}>
             Welcome! I'm Hannah!
           </Title>
-          <Text c="dimmed" mt="md">
+          <Text
+            className={`${classes.description} ${animate && classes.animate}`}
+            c="dimmed"
+            mt="md"
+          >
             I'm a first year CS student at the Georgia Institute of Technology!
             I am interested in the intersection of technology and social good! I
             am a CS 1331 TA, a Firmware Developer for Georgia Tech's Solar
@@ -33,37 +46,11 @@ export function HomePage() {
             purpose!
           </Text>
 
-          {/* <List */}
-          {/* mt={30}
-            spacing="sm"
-            size="sm" */}
-          {/* icon={
-              <ThemeIcon size={20} radius="xl">
-                <IconCheck
-                  style={{ width: rem(12), height: rem(12) }}
-                  stroke={1.5}
-                />
-              </ThemeIcon>
-            } */}
-          {/* > */}
-          {/* <List.Item>
-              <b>TypeScript based</b> – build type safe applications, all
-              components and hooks export types
-            </List.Item>
-            <List.Item>
-              <b>Free and open source</b> – all packages have MIT license, you
-              can use Mantine in any project
-            </List.Item>
-            <List.Item>
-              <b>No annoying focus ring</b> – focus ring will appear only when
-              user navigates with keyboard
-            </List.Item>
-          </List> */}
-
           <Group mt={30}>
             <Button
               //   variant="gradient"
               //   gradient={{ from: "yellow", to: "purple", deg: 90 }}
+              className={`${classes.buttonContainer} ${animate && classes.animate}`}
               onClick={() =>
                 window.open(
                   "https://drive.google.com/file/d/1OVu-M1r4TLKJXgZbRJUETWVfOE5zItTa/view?usp=sharing",
@@ -75,10 +62,9 @@ export function HomePage() {
             </Button>
           </Group>
         </div>
-        {/* Apply inline styles to set width and height */}
         <Image
           src={image}
-          className={classes.image}
+          className={`${classes.image} ${animate && classes.animate}`}
           style={{ width: rem(350), height: rem(350) }}
         />
       </div>
